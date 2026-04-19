035bd2acfd7b47ca9338ade942cd3b5bbc0e6ec3
itsanskrutischool <it.sanskrutischool@gmail.com>
2026-04-19 22:16:21 +0530

    Add Upstash Redis configuration

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -9,10 +9,11 @@
 let redis: any = null
 
 try {
-  if (process.env.REDIS_URL) {
+  if (process.env.REDIS_URL && process.env.UPSTASH_REDIS_REST_TOKEN) {
     const { Redis } = require("@upstash/redis")
     redis = new Redis({
       url: process.env.REDIS_URL,
+      token: process.env.UPSTASH_REDIS_REST_TOKEN,
     })
   }
 } catch (error) {
